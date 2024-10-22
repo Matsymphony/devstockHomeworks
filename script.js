8ca0c4625ec057f7ca140c1f78c47dd38a71a6c1
Mateusz Latkowski <mati199452@gmail.com>
2024-10-22 02:15:46 +0200

    Pierwszy Commit z plikami lokalnymi

---
+++ script.js
@@ -0,0 +1,15 @@
+const userName = "mateusz";
+const upperCaseName = userName.charAt(0).toUpperCase() + userName.substring(1);
+const userAge = 30;
+const likesJavaScript = true;
+const userHobby = "fotografia";
+const userCity = "Warszawa";
+const userAgeFuture = userAge + 10;
+const userAdult = userAge >= 18 ? "Jesteś pełnoletni" : "Nie jesteś pełnoletni";
+const isWarsawCitizen = userCity === "Warszawa";
+let likesJSAndIsAdult = userAge >= 18 && likesJavaScript === true;
+let message = `Witaj ${upperCaseName} masz ${userAge} lat. ${userAdult}.`;
+likesJavaScript === true && (message += ` Lubisz JavaScript.`);
+isWarsawCitizen === true && (message += ` Mieszkasz w Warszawie.`);
+message += ` Twoje hobby to ${userHobby}`;
+console.log(message);
